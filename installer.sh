9ae0ad0f546e487ddb61048986c64239942a21e2
Hasan2arabic <115946654+Hasan2arabic@users.noreply.github.com>
2024-03-09 12:40:25 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -12,8 +12,8 @@ echo "##########################################################################
 
 apt update && apt upgrade -y
 apt-get update && apt-get upgrade -y
-apt install sudo wget curl screen nano unzip git snap python3 nodejs npm docker.io docker-compose neofetch -y
-apt install systemd firewalld -y
+apt install sudo wget curl screen nano unzip git snap python3 nodejs npm docker.io docker-compose neofetch systemd firewalld iptables ssh-client ssh -y
+
 curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
   | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" \
   | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt install ngrok -y
